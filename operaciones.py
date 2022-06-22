277326aab951234436d80e92be1056f4ffac2dd4
Paula2409 <103932203+Paula2409@users.noreply.github.com>
2022-06-22 08:27:15 -0300

    Modulo de calculos para Tema 7

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+#Se crea el modulo para las operaciones
+
+def suma(a,b):
+  return a + b
+  
+def resta(a,b):
+  return a - b
+  
+def multiplicacion(a,b):
+  return a * b
+  
+def division(a,b):
+  return a / b
